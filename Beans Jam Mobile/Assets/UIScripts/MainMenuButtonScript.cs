33ac7ade1ed7461cedcbac7bff95434e2da103a4
simon-winter <sero93@aol.com>
2018-10-13 03:55:43 +0200

    BlablaMainMenu

---
+++ Beans Jam Mobile/Assets/UIScripts/MainMenuButtonScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuButtonScript : MonoBehaviour
+{    
+    public Animator anim;
+    int TriggerHash = Animator.StringToHash("TriggerMenuReset");
+    // Use this for initialization
+    void Start () {
+       
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void PlayLevel(int scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+
+    public void TriggerMenuBack()
+    {
+        anim.SetTrigger(TriggerHash);
+    }
+
+    public void TriggerMenuLevels()
+    {
+        anim.SetTrigger(TriggerHash);
+    }
+}
