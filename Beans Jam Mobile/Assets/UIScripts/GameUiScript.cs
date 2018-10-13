b8da75361bf725163b96c70c9304f8f7cf3f367b
simon-winter <you@example.com>
2018-10-13 17:01:39 +0200

    GameScreenUI initial

---
+++ Beans Jam Mobile/Assets/UIScripts/GameUiScript.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameUiScript : MonoBehaviour
+{
+
+    public RectTransform energyBar;
+    public RectTransform bluesBar;
+    public RectTransform barContainer;
+    public RectTransform noteAcceptanceArea;
+    public RectTransform noteBackgroundArea;
+    public GameObject notePrefab;
+    public Canvas canvas;
+
+    public float speed = 1;
+
+    public int SetEnergyBarFillPercentage;
+    public int SetBluesBarFillPercentage;
+
+    private float onePercentSize;
+    private float EnergyCurrentPerc = 100f;
+    private float BluesCurrentPerc = 10f;
+
+    private float EnergyDrainTimer = 1f;
+    public float EnergyDrainPeriod = 0.25f;
+
+    private Queue<string> rythmList; //= { 0:01:05 - 0:02:82, 0:04:58, };
+    private float startTime;
+
+    struct TimeStamp
+    {        
+        int min;
+        int sec;
+        int ms;
+    }
+
+    // Use this for initialization
+    void Start()
+    {        
+        float PercentageBarMaxWidth = barContainer.sizeDelta.x;
+        onePercentSize = (PercentageBarMaxWidth / 100f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > EnergyDrainTimer) {
+            EnergyDrainTimer += EnergyDrainPeriod;
+            IncreaseEnergy(-1);            
+        }
+    }
+    public void spawnRythm(Queue<bool> rythmQueue)
+    {
+        startTime = Time.time * 1000;
+        executeRythm(rythmQueue, startTime);
+    }
+    public void executeRythm(Queue<bool> rythmQueue, float startTime)
+    {
+        float elapsedMS = (Time.time * 1000) - startTime;
+        TimeStamp nextTimeStamp;
+        string[] splitTime = rythmList.Dequeue().Split(':');
+        nextTimeStamp splitTime[0]
+    }
+    public void spawnNote(float speed)
+    {
+        float xpos = noteBackgroundArea.position.x;
+        float ypos = noteBackgroundArea.position.y;
+        float zpos = noteBackgroundArea.position.z;
+
+        (Instantiate(notePrefab, new Vector3(xpos, ypos, zpos), Quaternion.identity, canvas.transform) as GameObject).GetComponent<NoteBehavior>().InitNoteSpeed(speed); 
+    }
+
+    public void IncreaseEnergy(float percentageToAdd)
+    {
+        float perc = EnergyCurrentPerc + percentageToAdd;
+        if (perc > 100f)
+            perc = 100f;
+        else if (perc < 0f)
+            perc = 0f;
+
+        energyBar.sizeDelta = new Vector2(perc * onePercentSize, energyBar.sizeDelta.y);
+        EnergyCurrentPerc = perc;
+    }
+    public void IncreaseBlues(float percentageToAdd)
+    {
+        float perc = BluesCurrentPerc + percentageToAdd;
+        if (perc > 100f)
+            perc = 100f;
+        else if (perc < 0f)
+            perc = 0f;
+
+        bluesBar.sizeDelta = new Vector2(perc * onePercentSize, bluesBar.sizeDelta.y);
+        BluesCurrentPerc = perc;
+    }
+}
+
---
+++ Beans Jam Mobile/Assets/UIScripts/NoteBehavior.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteBehavior : MonoBehaviour {
+
+    private float noteSpeed;
+    private RectTransform tf;
+
+	// Use this for initialization
+	void Start () {
+        tf = gameObject.GetComponent<RectTransform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        tf.position = new Vector3(tf.position.x + (noteSpeed*Time.deltaTime), tf.position.y, tf.position.z);
+	}  
+
+    public void InitNoteSpeed(float speed)
+    {
+        noteSpeed = speed;
+    }
+
+    void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+}
