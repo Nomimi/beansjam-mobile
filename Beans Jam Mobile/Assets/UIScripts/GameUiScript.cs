e89ac4caf47e8a3e293cd3d427a4e26f10eb4531
simon-winter <you@example.com>
2018-10-13 18:03:55 +0200

    TimeStampParser integrated

--- Beans Jam Mobile/Assets/UIScripts/GameUiScript.cs
+++ Beans Jam Mobile/Assets/UIScripts/GameUiScript.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -25,19 +26,62 @@ public class GameUiScript : MonoBehaviour
     private float EnergyDrainTimer = 1f;
     public float EnergyDrainPeriod = 0.25f;
 
-    private Queue<string> rythmList; //= { 0:01:05 - 0:02:82, 0:04:58, };
+    private Queue<string> timingsList; //= { 0:01:05 - 0:02:82, 0:04:58, };
+    private Queue<TimeStamp> timeStamps;
     private float startTime;
 
     struct TimeStamp
     {        
-        int min;
-        int sec;
-        int ms;
+        public int min;
+        public int sec;
+        public int ms;
+
+        public TimeStamp(float milliseconds) : this()
+        {
+            min = (int)milliseconds / 60000;
+            milliseconds -= min * 60000;
+            sec = (int)milliseconds / 1000;
+            milliseconds -= sec * 1000;
+            ms = (int)milliseconds % 1000;
+            if (ms > 100)
+                ms /= 10;
+        }
+        public static bool operator >=(TimeStamp t1, TimeStamp t2)
+        {
+            if (t1.min >= t2.min)
+                if (t1.sec >= t2.sec)
+                    if (t1.ms >= t2.ms)
+                        return true;
+            return false;
+        }
+        public static bool operator <=(TimeStamp t1, TimeStamp t2)
+        {
+            if (t1.min <= t2.min)
+                if (t1.sec <= t2.sec)
+                    if (t1.ms <= t2.ms)
+                        return true;
+            return false;
+        }
+
+        public override string ToString()
+        {
+            return min+":" + sec + ":" + ms;
+        }
     }
 
     // Use this for initialization
     void Start()
-    {        
+    {
+        TimeStamp a1 = new TimeStamp(50f);
+        TimeStamp a2 = new TimeStamp(1050f);
+        TimeStamp a3 = new TimeStamp(2500f);
+        TimeStamp a4 = new TimeStamp(62500f);
+        timeStamps = new Queue<TimeStamp>();
+        Debug.Log(a1 + "\n" );
+        Debug.Log(a2 + "\n" );
+        Debug.Log(a3 + "\n");
+        Debug.Log(a4 + "\n");
+
         float PercentageBarMaxWidth = barContainer.sizeDelta.x;
         onePercentSize = (PercentageBarMaxWidth / 100f);
     }
@@ -49,18 +93,31 @@ public class GameUiScript : MonoBehaviour
             EnergyDrainTimer += EnergyDrainPeriod;
             IncreaseEnergy(-1);            
         }
+        if(timeStamps.Count > 0) {
+           // (Time.time * 1000 - startTime).ToString();
+
+           // TimeStamp now =
+          // if(( >= timeStamps.Peek())
+
+        }
     }
     public void spawnRythm(Queue<bool> rythmQueue)
     {
         startTime = Time.time * 1000;
+        foreach (string timing in timingsList) {
+            TimeStamp crtTimeStamp = new TimeStamp();
+            string[] splitTime = timingsList.Dequeue().Split(':');
+            crtTimeStamp.min = Int32.Parse(splitTime[0]);
+            crtTimeStamp.sec = Int32.Parse(splitTime[1]);
+            crtTimeStamp.ms = Int32.Parse(splitTime[2]);
+            timeStamps.Enqueue(crtTimeStamp);
+        }
         executeRythm(rythmQueue, startTime);
     }
     public void executeRythm(Queue<bool> rythmQueue, float startTime)
     {
-        float elapsedMS = (Time.time * 1000) - startTime;
-        TimeStamp nextTimeStamp;
-        string[] splitTime = rythmList.Dequeue().Split(':');
-        nextTimeStamp splitTime[0]
+       
+        
     }
     public void spawnNote(float speed)
     {
