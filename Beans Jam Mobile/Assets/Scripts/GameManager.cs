1d5aa8a7090462b89b6afb98bfd037ba0ade3414
Dreadcrumb <leander.suda@aon.at>
2018-10-13 22:14:16 +0200

    new hunger system

--- Beans Jam Mobile/Assets/Scripts/GameManager.cs
+++ Beans Jam Mobile/Assets/Scripts/GameManager.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Security.Policy;
 using UnityEngine;
-using UnityEngine.PostProcessing;
 using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
@@ -126,90 +125,86 @@ public class GameManager : MonoBehaviour
 			particleSystem.Play();
 		}
 	}
-}
 
-//Method to Return Clicked Object
-GameObject ReturnClickedObject(out RaycastHit hit)
-{
-	GameObject target = null;
-	Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-	if (Physics.Raycast(ray.origin, ray.direction * 10, out hit))
+	//Method to Return Clicked Object
+	GameObject ReturnClickedObject(out RaycastHit hit)
 	{
-		target = hit.collider.gameObject;
-	}
-	return target;
-}
-
-//Method to Return Clicked Object
-GameObject ReturnClickedObject(Touch t, out RaycastHit hit)
-{
-	GameObject target = null;
-	Ray ray = Camera.main.ScreenPointToRay(t.position);
-	if (Physics.Raycast(ray.origin, ray.direction * 10, out hit))
-	{
-		target = hit.collider.gameObject;
-	}
-	return target;
-}
-
-IEnumerator ApplyHunger()
-{
-	for (float i = 100; i >= 0; i -= Hunger)
-	{
-		Saturation -= Hunger;
-		i = Saturation;
-		// TODO set UI 
-		// TODO set Dino Model
-		yield return null;
-	}
-	// i >= 0
-	Running = false;
-	// Load Game-Over Screen
-	_gameOverText.CrossFadeAlpha(1f, 2f, true);
-	StartCoroutine("TransitionToGameOver");
-}
-
-void OnCollisionEnter(Collider c)
-{
-	if (c.CompareTag("MeatBag"))
-	{
-		Saturation += 10;
-		Destroy(c.gameObject);
-
-	}
-}
-
-Vector3 GetSpawnPoint()
-{
-	Vector3 spawnPoint = _spawnArea.transform.position;
-	if (Random.value < 0.5f)
-	{
-		// Spawn left
-		spawnPoint -= _spawnArea.transform.right * SpawnOffset;
-	}
-	else
-	{
-		// Spawn right
-		spawnPoint += _spawnArea.transform.right * SpawnOffset;
-	}
-	return spawnPoint;
-}
-
-public void RemoveMeatbag(GameObject meatBag)
-{
-	_meatBags.Remove(meatBag);
-}
-
-IEnumerator TransitionToGameOver()
-{
-	var cam = Camera.main;
-	for (float i = 0; i <= 150; i += 1.5f)
-	{
-
-		cam.focalLength = i;
-
-		yield return null;
+		GameObject target = null;
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		if (Physics.Raycast(ray.origin, ray.direction * 10, out hit))
+		{
+			target = hit.collider.gameObject;
+		}
+		return target;
 	}
 
-}
+	//Method to Return Clicked Object
+	GameObject ReturnClickedObject(Touch t, out RaycastHit hit)
+	{
+		GameObject target = null;
+		Ray ray = Camera.main.ScreenPointToRay(t.position);
+		if (Physics.Raycast(ray.origin, ray.direction * 10, out hit))
+		{
+			target = hit.collider.gameObject;
+		}
+		return target;
+	}
+
+	IEnumerator ApplyHunger()
+	{
+		for (float i = 100; i >= 0; i -= Hunger)
+		{
+			Saturation -= Hunger;
+			i = Saturation;
+			// TODO set UI 
+			// TODO set Dino Model
+			yield return null;
+		}
+		// i >= 0
+		Running = false;
+		// Load Game-Over Screen
+		_gameOverText.CrossFadeAlpha(1f, 2f, true);
+		StartCoroutine("TransitionToGameOver");
+	}
+
+	void OnCollisionEnter(Collider c)
+	{
+		if (c.CompareTag("MeatBag"))
+		{
+			Saturation += 10;
+			Destroy(c.gameObject);
+
+		}
+	}
+
+	Vector3 GetSpawnPoint()
+	{
+		Vector3 spawnPoint = _spawnArea.transform.position;
+		if (Random.value < 0.5f)
+		{
+			// Spawn left
+			spawnPoint -= _spawnArea.transform.right * SpawnOffset;
+		}
+		else
+		{
+			// Spawn right
+			spawnPoint += _spawnArea.transform.right * SpawnOffset;
+		}
+		return spawnPoint;
+	}
+
+	public void RemoveMeatbag(GameObject meatBag)
+	{
+		_meatBags.Remove(meatBag);
+	}
+
+	IEnumerator TransitionToGameOver()
+	{
+		var cam = Camera.main;
+		for (float i = 0; i <= 100; i += 1.5f)
+		{
+			cam.focalLength++; 
+			yield return null;
+		}
+	}
 }
